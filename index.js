81cf23ccd7b255b8c594d70014ec38a5e7587c22
Dev Vanilla <mgvanilla@gmail.com>
2023-04-09 17:56:22 +0300

    Create title

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
 #!/usr/bin/env node
 
 import inquirer from 'inquirer';
+import figlet from 'figlet';
+import chalk from 'chalk';
+import gradient from 'gradient-string';
+
+
+const categorie = ["FFPM", "FIHIRANA FANAMPINY", "ANTEMA"]
+const title = "FIHIRANAKO"
+
+//TimeOut 2s
+const sleep = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
+const log = console.log;
+
+async function welcome() {
+    console.clear();
+    figlet(`NY FIHIRANAKO`, (err, data) => {
+        console.log(gradient.pastel.multiline(data) + '\n');
+        process.exit(0);
+    });
+
+
+    await sleep();
+
+    log("\n \n \n \n \nTongasoa eto @ Ny Fihiranako")
+}
+
+
+async function main() {
+    console.clear();
+    await welcome();
+
+}
+
+//main();
+main()
